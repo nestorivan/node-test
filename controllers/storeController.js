be9f4b5178c70f629c49a0b46148d65c3edc6d00
nestorivan <nestor.lopez.delgadillo@gmail.com>
2018-11-18 01:28:50 -0600

    add editStore method

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -17,6 +17,22 @@ exports.createStore = async (req, res) => {
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
-  console.log(stores);
   res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+
+  req.flash('success', `Successfuly updayed <strong>${store.name}</strong>. <a href="stores/${store.slug}">View Store</a>`);
+
+  res.redirect(`/stores/${store._id}/edit`);
 };
