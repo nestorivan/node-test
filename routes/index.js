f0827c698710115d63ac443e05bf9cc916a23c9c
nestorivan <nestor.lopez.delgadillo@gmail.com>
2018-11-19 00:18:51 -0600

    add singleStore route

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,21 @@ const router = express.Router();
 const storeController = require('./../controllers/storeController');
 const { catchErrors } = require('./../handlers/errorHandlers');
 
-// Do work here
+// get
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/add', storeController.addStore);
+router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
 
+//post
 router.post(
   '/add',
   storeController.upload,
   catchErrors(storeController.resize),
   catchErrors(storeController.createStore)
 );
+
 router.post(
   '/add/:id',
   storeController.upload,
